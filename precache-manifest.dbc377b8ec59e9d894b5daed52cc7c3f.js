0f0db2e72efd29ec8180af14170ccbbfbb307f85
kabir <kabir.almuslim@gmail.com>
2022-09-04 09:43:25 -0600

    Deploy React app to GitHub Pages

---
+++ precache-manifest.dbc377b8ec59e9d894b5daed52cc7c3f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2a0e4de57096a05578f4",
+    "url": "/reactapp/static/css/main.5f776bc8.chunk.css"
+  },
+  {
+    "revision": "2a0e4de57096a05578f4",
+    "url": "/reactapp/static/js/main.2a0e4de5.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/reactapp/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "946d8365c0317c9ba6aa",
+    "url": "/reactapp/static/js/2.946d8365.chunk.js"
+  },
+  {
+    "revision": "b82631e63b24b0f31f18",
+    "url": "/reactapp/static/js/runtime~main.b82631e6.js"
+  },
+  {
+    "revision": "2f6b3f67d986f063564967e87a2fbbc4",
+    "url": "/reactapp/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Textutils/precache-manifest.56bfefb9d2bb85745b38cfcbbdc14252.js"
+  "/reactapp/precache-manifest.dbc377b8ec59e9d894b5daed52cc7c3f.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/Textutils/index.html", {
+workbox.routing.registerNavigationRoute("/reactapp/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
